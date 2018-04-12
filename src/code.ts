fe5d7fc86345267efadb6456452b0d9dd5183132
siddharthasarma <sidduinevitable@gmail.com>
2018-04-12 17:14:41 +0530

    Adding working code

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,26 @@
-import * as Rx from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
-console.log(Rx);
+const observable = Observable.create((observer: any) => {
+  try {
+    observer.next('say Hi');
+    observer.next('Hi there');
+    observer.next('How are you?');
+    observer.complete();
+    observer.next('This will not be shown');
+  } catch (error) {
+    observer.error(error);
+  }
+});
+
+observable.subscribe(
+  (val: any) => addItem(val),
+  (error: any) => addItem(error),
+  () => addItem('completed')
+);
+
+function addItem(val: string): any {
+  var node = document.createElement('li');
+  var textNode = document.createTextNode(val);
+  node.appendChild(textNode);
+  document.getElementById('output').appendChild(node);
+}
