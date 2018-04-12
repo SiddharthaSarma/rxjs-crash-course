8286602e913dcb07be798de0adb07e033d2acf99
siddharthasarma <sidduinevitable@gmail.com>
2018-04-12 17:18:01 +0530

    Add observer unsubscribe method

--- src/code.ts
+++ src/code.ts
@@ -5,18 +5,20 @@ const observable = Observable.create((observer: any) => {
     observer.next('say Hi');
     observer.next('Hi there');
     observer.next('How are you?');
-    observer.complete();
-    observer.next('This will not be shown');
+    setInterval(() => observer.next('I am good'), 2000);
   } catch (error) {
     observer.error(error);
   }
 });
 
-observable.subscribe(
+const observer = observable.subscribe(
   (val: any) => addItem(val),
   (error: any) => addItem(error),
   () => addItem('completed')
 );
+
+// Unsubscribing from observer
+setTimeout(() => observer.unsubscribe(), 6001);
 
 function addItem(val: string): any {
   var node = document.createElement('li');
