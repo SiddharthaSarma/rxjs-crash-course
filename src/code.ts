5db84f3ea7af382949911804fdac5aec8ddb8efa
siddharthasarma <sidduinevitable@gmail.com>
2018-04-12 17:32:21 +0530

    Add another subscriber to the observable

--- src/code.ts
+++ src/code.ts
@@ -11,18 +11,35 @@ const observable = Observable.create((observer: any) => {
   }
 });
 
+// First subscriber
 const observer = observable.subscribe(
-  (val: any) => addItem(val),
+  (val: string) => addItem(val),
   (error: any) => addItem(error),
   () => addItem('completed')
 );
 
+// second subscriber
+const secondObserver = observable.subscribe((val: string) =>
+  addAnotherItem(val)
+);
+
+// to make the second subscriber work along with the first subscriber
+observer.add(secondObserver); // comment out this line and see the difference
+
 // Unsubscribing from observer
 setTimeout(() => observer.unsubscribe(), 6001);
 
-function addItem(val: string): any {
+// to add the item to the DOM.
+function addItem(val: string): void {
   var node = document.createElement('li');
   var textNode = document.createTextNode(val);
   node.appendChild(textNode);
   document.getElementById('output').appendChild(node);
+}
+// to add the item to the DOM.
+function addAnotherItem(val: string): void {
+  var node = document.createElement('li');
+  var textNode = document.createTextNode(val);
+  node.appendChild(textNode);
+  document.getElementById('anotherOutput').appendChild(node);
 }
